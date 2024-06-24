ff2da18ff62040184b472365d0c57980d1641f18
tahanlong <1066203862@qq.com>
2024-06-24 20:22:50 +0800

    增加对claude-3-5-sonnet-20240620支持
    deepseek-coder也是不错的选择

--- config.py
+++ config.py
@@ -36,6 +36,8 @@ AVAIL_LLM_MODELS = ["gpt-4-1106-preview", "gpt-4-turbo-preview", "gpt-4-vision-p
                     "gpt-4o", "gpt-4-turbo", "gpt-4-turbo-2024-04-09",
                     "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-3.5-turbo", "azure-gpt-3.5",
                     "gpt-4", "gpt-4-32k", "azure-gpt-4", "glm-4", "glm-4v", "glm-3-turbo",
+                    "deepseek-coder",
+                    "claude-3-5-sonnet-20240620","claude-3-haiku-20240307","claude-3-sonnet-20240229","claude-3-opus-20240229", 
                     "gemini-pro", "chatglm3"
                     ]
 # --- --- --- ---
